9374d66ec32712a3b74cdaf519850d057dae0f22
deepanshu verma <deepanshuverma186@gmail.com>
2025-12-09 13:04:39 +0530

    classes in typescript with custom methods

--- classes/dist/classes.js
+++ classes/dist/classes.js
@@ -1,17 +1,36 @@
 "use strict";
+// class dataManager {
+//    static myData:number[] = []
+//    static pushData(data:number){
+//         this.myData.push(data)
+//     }
+//     static getData(){
+//         console.log(this.myData)
+//     }
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-class dataManager {
-    static myData = [];
-    static pushData(data) {
-        this.myData.push(data);
+// dataManager.pushData(10)
+// dataManager.pushData(20)
+// dataManager.pushData(30)
+// dataManager.pushData(40)
+// dataManager.getData()
+class Intro {
+    name;
+    email;
+    password;
+    constructor(name, email, password) {
+        this.name = name,
+            this.email = email,
+            this.password = password;
     }
-    static getData() {
-        console.log(this.myData);
+    sayName() {
+        console.log(`${this.name}`);
+    }
+    sayEmail() {
+        console.log(`${this.email}`);
     }
 }
-dataManager.pushData(10);
-dataManager.pushData(20);
-dataManager.pushData(30);
-dataManager.pushData(40);
-dataManager.getData();
+const newUser = new Intro("deepanshu", "deepanshu@gmail.com", "12334444");
+newUser.sayName();
+newUser.sayEmail();
 //# sourceMappingURL=classes.js.map
--- classes/src/classes.ts
+++ classes/src/classes.ts
@@ -1,24 +1,50 @@
+// class dataManager {
+//    static myData:number[] = []
+//    static pushData(data:number){
+//         this.myData.push(data)
+//     }
+//     static getData(){
+//         console.log(this.myData)
+//     }
+// }
+
+
+
+// dataManager.pushData(10)
+// dataManager.pushData(20)
+// dataManager.pushData(30)
+// dataManager.pushData(40)
+
+
+// dataManager.getData()
 
 
 
 
-class dataManager {
-   static myData:number[] = []
-   static pushData(data:number){
-        this.myData.push(data)
+
+
+class Intro  {
+     name:string
+     email:string
+     password:string
+
+    constructor(name:string,email:string,password:string){
+        this.name = name,
+        this.email = email,
+        this.password = password 
     }
-    static getData(){
-        console.log(this.myData)
-    }
+     sayName(){
+        console.log(`${this.name}`)
+     }  
+     sayEmail(){
+        console.log(`${this.email}`)
+     }
+
 }
 
+const newUser = new Intro("deepanshu","deepanshu@gmail.com","12334444")
 
+newUser.sayName()
+newUser.sayEmail()
 
-dataManager.pushData(10)
-dataManager.pushData(20)
-dataManager.pushData(30)
-dataManager.pushData(40)
-
-
-dataManager.getData()
 
