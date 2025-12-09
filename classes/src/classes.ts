57586dc5576206521350d09a886c461efdb7c547
deepanshu verma <deepanshuverma186@gmail.com>
2025-12-09 15:32:33 +0530

    inheritence implements in classes

--- classes/src/classes.ts
+++ classes/src/classes.ts
@@ -1,51 +1,51 @@
-// class dataManager {
-//    static myData:number[] = []
-//    static pushData(data:number){
-//         this.myData.push(data)
-//     }
-//     static getData(){
-//         console.log(this.myData)
-//     }
-// }
+class dataManager {
+   static myData:number[] = []
+   static pushData(data:number){
+        this.myData.push(data)
+    }
+    static getData(){
+        console.log(this.myData)
+    }
+}
 
 
 
-// dataManager.pushData(10)
-// dataManager.pushData(20)
-// dataManager.pushData(30)
-// dataManager.pushData(40)
+dataManager.pushData(10)
+dataManager.pushData(20)
+dataManager.pushData(30)
+dataManager.pushData(40)
 
 
-// dataManager.getData()
-
-
-
+dataManager.getData()
 
 
 
-// class Intro  {
-//      name:string
-//      email:string
-//      password:string
 
-//     constructor(name:string,email:string,password:string){
-//         this.name = name,
-//         this.email = email,
-//         this.password = password 
-//     }
-//      sayName(){
-//         console.log(`${this.name}`)
-//      }  
-//      sayEmail(){
-//         console.log(`${this.email}`)
-//      }
 
-// }
 
-// const newUser = new Intro("deepanshu","deepanshu@gmail.com","12334444")
+class Intro  {
+     name:string
+     email:string
+     password:string
 
-// newUser.sayName()
-// newUser.sayEmail()
+    constructor(name:string,email:string,password:string){
+        this.name = name,
+        this.email = email,
+        this.password = password 
+    }
+     sayName(){
+        console.log(`${this.name}`)
+     }  
+     sayEmail(){
+        console.log(`${this.email}`)
+     }
+
+}
+
+const newUser = new Intro("deepanshu","deepanshu@gmail.com","12334444")
+
+newUser.sayName()
+newUser.sayEmail()
 
 
 
@@ -68,13 +68,13 @@ class Person {
        return this.name
     }
 
-   //  changeName(){
-   //    this.name = "deepanshu"
-   //  }
+    changeName(){
+      this.name = "deepanshu"
+    }
 
    // we cannot change its name because its readable only
 
-    // const newVal = new Person("dee")
+    const newVal = new Person("dee")
     
 }
 
@@ -85,27 +85,32 @@ console.log(person1.getName())
 
 /// Inheritance Implements 
 
-// interface Shape {
-//    getArea:()=>{}
-// }
+interface Shape {
+   getArea:()=>{}
+}
 
 
-// class Rectangle implements Shape {
-//      constructor(protected readonly width:number,protected readonly height:number){
-//       this.height = height,
-//       this.width = width
-//      }
+class Rectangle implements Shape {
+     constructor(protected readonly width:number,protected readonly height:number){
+      this.height = height,
+      this.width = width
+     }
 
-//      public getArea():number{
-//        return this.width * this.height
-//      }
+     public getArea():number{
+       return this.width * this.height
+     }
     
-// }
+}
 
+class Square extends Rectangle{
+   public constructor(width:number){
+      super(width,width)
+   }
+}
 
-// const rect = new Rectangle(10,20)
+const rect = new Rectangle(10,20)
 
-// console.log(rect.getArea())
+console.log(rect.getArea())
 
 
 
